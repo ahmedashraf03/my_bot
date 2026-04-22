4b9c04a9ade27bd9ebeea7d46da12dc6bf8ec7c6
ahmedashraf03 <ahmedashraf2hamdy@gmail.com>
2026-04-22 13:37:45 +0200

    my local changes

--- launch/rplidar.launch.py
+++ launch/rplidar.launch.py
@@ -11,7 +11,11 @@ def generate_launch_description():
             executable='rplidar_composition',
             output='screen',
             parameters=[{
+<<<<<<< HEAD
                 'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port0',
+=======
+                'serial_port': '/dev/serial/by-path/platform-fd500000.pcie-pci-0000:01:00.0-usb-0:1.3:1.0-port',
+>>>>>>> 64eefd2 (my local changes)
                 'frame_id': 'laser_frame',
                 'angle_compensate': True,
                 'scan_mode': 'Standard',
